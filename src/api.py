17f61ff991135291d029835c839f626d4a8ae84f
Denis Strizhkin <strdenis02@gmail.com>
2023-05-14 06:01:10 +0300

    room functions

--- src/api.py
+++ src/api.py
@@ -1,6 +1,10 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from functools import wraps
+import jwt
+import datetime
 
 
 # Flask app
@@ -22,6 +26,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = \
     f'@{DB_HOST}:{DB_PORT}/{POSTGRES_DB}'
 
 db = SQLAlchemy(app)
+
 
 """
 
@@ -66,14 +71,232 @@ db = SQLAlchemy(app)
 
 """
 
+
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), nullable=False)
     email = db.Column(db.String(50), nullable=False, unique=True)
-    password = db.Column(db.String(102), nullable=False)
+    password = db.Column(db.String(200), nullable=False)
     photo_url = db.Column(db.String(100))
+
+    rooms = db.relationship('Room', backref='user')
+    members = db.relationship('Members', backref='user')
+
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), nullable=False)
+    owner_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        nullable = False
+    )
+
+    members = db.relationship('Members', backref='room')
+
+
+class Members(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    member_id = db.Column(
+        db.Integer,
+        db.ForeignKey('user.id', ondelete='CASCADE'),
+        nullable = False
+    )
+    room_id = db.Column(
+        db.Integer,
+        db.ForeignKey('room.id', ondelete='CASCADE'),
+        nullable = False
+    )
+    __table_args__ = (db.UniqueConstraint('member_id', 'room_id'),)
 
 
 def init_db():
     db.drop_all()
     db.create_all()
+
+
+def verify_json_fields(json, fields):
+    for field in fields:
+        if not field in json:
+            return False
+    return True
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+
+        if 'x-access-token' in request.headers:
+            token = request.headers['x-access-token']
+
+        if not token:
+            return jsonify({ 'msg' : 'no token provided' }), 400
+
+        try:
+            json = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            user = User.query.filter_by(email=json['email']).first()
+        except:
+            return jsonify({ 'msg' : 'token is invalid' }), 400
+
+        return f(user, *args, **kwargs)
+
+    return decorated
+
+
+def fields_required(fields):
+    def decorator(f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            json = request.get_json()
+            if not verify_json_fields(json, fields):
+                return jsonify({ 'msg' : 'invalid fields'}), 400
+
+            return f(json, *args, **kwargs)
+        
+        return decorated
+    
+    return decorator
+
+
+@app.route('/register', methods=['POST'])
+@fields_required(['email', 'name', 'password'])
+def register(json):
+    password = generate_password_hash(json['password'], method='scrypt')
+    new_user = User(
+        name=json['name'],
+        email=json['email'],
+        password=password
+    )
+
+    if User.query.filter_by(email=json['email']).first():
+        return jsonify({ 'msg' : 'user already exists' }), 400
+    
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({ 'msg' : 'user created' })
+
+
+@app.route('/login', methods=['POST'])
+@fields_required(['email', 'password'])
+def login(json):
+    user = User.query.filter_by(email=json['email']).first()
+    if not user:
+        return jsonify({ 'msg' : 'user does not exist' }), 400
+
+    if not check_password_hash(user.password, json['password']):
+        return jsonify({ 'msg' : 'wrong password' }), 400
+
+    token = jwt.encode({
+        'email' : user.email,
+        'exp' : datetime.datetime.utcnow() + datetime.timedelta(days=30)
+    }, app.config['SECRET_KEY'], algorithm="HS256")
+
+    return jsonify({ 'token' : token, 'id' : user.id })
+
+
+@app.route('/auth', methods=['GET'])
+@token_required
+def auth(user):
+    return jsonify({ 'msg' : f'access granted for {user.email}' })
+
+
+@app.route('/delete_user', methods=['GET'])
+@token_required
+def delete_user(user):
+    user = User.query.filter_by(email=user.email).first()
+    db.session.delete(user)
+    db.session.commit()
+
+    return jsonify({ 'msg' : f'user deleted - {user.email}' })
+
+
+@app.route('/add_room', methods=['POST'])
+@token_required
+@fields_required(['name'])
+def add_room(json, user):
+    room = Room(name=json['name'], owner_id=user.id)
+    db.session.add(room)
+    db.session.commit()
+
+    return jsonify({ 'msg' : 'room created', 'id' : room.id })
+
+
+@app.route('/delete_room', methods=['POST'])
+@token_required
+@fields_required(['id'])
+def delete_room(json, user):
+    room = Room.query.filter_by(id=json['id']).first()
+    if not room or room.owner_id != user.id:
+        return jsonify({ 'msg' : f'user does have room with id - {json["id"]}' }), 400
+    
+    db.session.delete(room)
+    db.session.commit()
+
+    return jsonify({ 'msg' : f'room deleted - {json["id"]}' }) 
+
+
+@app.route('/get_rooms_admin', methods=['GET'])
+@token_required
+def get_rooms_admin(user):
+    rooms = []
+    for room in user.rooms:
+        rooms.append({
+            'id' : room.id,
+            'name' : room.name,
+            'owner_id' : room.owner_id
+        })
+
+    return jsonify({ 'msg' : rooms })
+
+
+@app.route('/get_rooms_member', methods=['GET'])
+@token_required
+def get_rooms_member(user):
+    rooms = []
+    for member in user.members:
+        room = Room.query.filtery_by(id=member.room_id).first()
+        rooms.append({
+            'id' : room.id,
+            'name' : room.name,
+            'owner_id' : room.owner_id
+        })
+    return jsonify({ 'msg' : rooms })
+
+
+"""
+@app.route('get_rooms', methods=['GET'])
+@token_required
+def get_rooms(user):
+    return ''
+"""
+
+
+@app.route('/join_room', methods=['POST'])
+@token_required
+@fields_required(['id'])
+def join_room(json, user):
+    member = Members.query.filter_by(member_id=user.id, room_id=json['id']).first()
+    if member:
+        return jsonify({ 'msg' : 'this user have already joined' }), 400
+
+    member = Members(member_id=user.id, room_id=json['id'])
+    db.session.add(member)
+    db.session.commit()
+
+    return jsonify({ 'msg' : 'user have joined the room' })
+
+
+@app.route('/leave_room', methods=['POST'])
+@token_required
+@fields_required(['id'])
+def leave_room(json, user):
+    member = Members.query.filter_by(member_id=user.id, room_id=json['id']).first()
+    if not member:
+        return jsonify({ 'msg' : 'user haven\'t joined this room' }), 400
+
+    db.session.delete(member)
+    db.session.commit()
+
+    return jsonify({ 'msg' : 'user have left the room' })
---
+++ src/api_test.py
@@ -0,0 +1,256 @@
+#!/bin/python
+
+import requests
+import json
+
+
+API_URL = "http://localhost:8080"
+
+REGISTER_URL = API_URL + "/register"
+LOGIN_URL= API_URL + "/login"
+AUTH_URL = API_URL + "/auth"
+DELETE_USER_URL= API_URL + "/delete_user"
+
+ADD_ROOM_URL = API_URL + "/add_room"
+DELETE_ROOMS_URL = API_URL + "/delete_room"
+
+GET_ROOMS_ADMIN_URL = API_URL + "/get_rooms_admin"
+GET_ROOMS_MEMBER_URL = API_URL + "/get_rooms_member"
+GET_ROOMS_URL = API_URL + "/get_rooms"
+
+JOIN_ROOM_URL = API_URL + "/join_room"
+DELETE_MEMBER_URL = API_URL + "/delete_member"
+
+ADD_CHEQUE_URL = API_URL + "/add_cheque"
+GET_CHEQUES_URL = API_URL + "/get_cheques"
+DELETE_CHEQUE_URL = API_URL + "/delete_cheque"
+
+
+#MAX_USERNAME = 32
+#MIN_USERNAME = 4
+#
+#MAX_PASSWORD = 16
+#MIN_PASSWORD = 8
+#
+#MAX_EMAIL = 40
+
+
+def create_user(letter):
+    data = {
+        'email': f'{letter}@test.com',
+        'name': f'{letter}',
+        'password': 'password'
+    }
+    return data, requests.post(REGISTER_URL, json=data)
+
+
+def get_token(user):
+    data = {
+        'email': user['email'],
+        'password': user['password']
+    }
+    x = requests.post(LOGIN_URL, json=data)
+    info = json.loads(x.text)
+
+    token = None
+    if 'token' in info:
+        token = info['token']
+    return token, x
+
+
+def delete_user(token):
+    return requests.get(DELETE_USER_URL, headers={'x-access-token':token})
+
+
+def add_room(token, name):
+    data = { 'name' : name }
+    x = requests.post(ADD_ROOM_URL, headers={'x-access-token':token}, json=data)
+    info = json.loads(x.text)
+    id = None
+    if 'id' in info:
+        id = info['id']
+    return id, x
+
+
+def delete_room(token, id):
+    data = { 'id' : id }
+    x = requests.post(DELETE_ROOMS_URL, headers={'x-access-token':token}, json=data)
+    return x
+
+
+def test_register_and_login():
+    print('### test authentification ###')
+    user, user_x = create_user('A')
+    print(user_x.text)
+    token, token_x = get_token(user)
+    print(token_x.text)
+    x = requests.get(AUTH_URL, headers={'x-access-token':token})
+    print(x.text)
+    x = delete_user(token)
+    print(x.text)
+
+
+def test_add_rooms():
+    print('### test add rooms (get_rooms_admin) ###')
+    
+    user, user_x = create_user('A')
+    token, token_x = get_token(user)
+    
+    room1, room1_x = add_room(token, 'aaa')
+    print(room1_x.text)
+    room2, room2_x = add_room(token, 'bbb')
+    print(room2_x.text)
+
+    x = requests.get(GET_ROOMS_ADMIN_URL, headers={'x-access-token':token})
+    print(x.text)
+    
+    x = delete_room(token, room1)
+    print(x.text)
+    
+    x = requests.get(GET_ROOMS_ADMIN_URL, headers={'x-access-token':token})
+    print(x.text)
+
+    delete_user(token)
+
+
+def test_join_room():
+    print('### test_join_rooms (get_rooms_member) ###')
+
+    resp = create_user('a')
+    (id_a, token_a) = get_token('a')
+    
+    resp = create_user('b')
+    (id_b, token_b) = get_token('b')
+    
+    data = {
+        'token': token_a,
+        'name': 'room'
+    }
+    x = requests.post(ADD_ROOM_URL, json=data)
+    print(x.status_code, x.text)
+    x = requests.post(GET_ROOMS_ADMIN_URL, json=data)
+    print(x.status_code, x.text)
+
+    room_id = json.loads(x.text)['message'][0]['id']
+    data = {
+        'token': token_b,
+        'id': room_id
+    }
+    x = requests.post(JOIN_ROOM_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': token_a,
+        'name': 'room'
+    }
+    x = requests.post(GET_ROOMS_ADMIN_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': token_a,
+        'room_id': room_id,
+        'user_id': id_b
+    }
+    x = requests.post(DELETE_MEMBER_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': token_a,
+    }
+    x = requests.post(DELETE_ROOMS_URL, json=data)
+    print(x.status_code, x.text)
+    
+    data = {
+        'token': token_a,
+        'id' : id_a
+    }
+    requests.post(DELETE_URL, json=data)
+    
+    data = {
+        'token': token_b,
+        'id' : id_b
+    }
+    requests.post(DELETE_URL, json=data)
+
+
+def test_add_cheques():
+    print('### test_add_cheques ###')
+
+    data = {
+        'email': 'admin@test.com',
+        'username': 'admin',
+        'password': 'adminadmin'
+    }
+    requests.post(REGISTER_URL, json=data)
+    
+    data = {
+        'email': 'admin@test.com',
+        'password': 'adminadmin'
+    }
+    x = requests.post(LOGIN_URL, json=data)
+    info = json.loads(x.text)
+
+    data = {
+        'token': info['token'],
+        'name': 'room1'
+    }
+    x = requests.post(ADD_ROOM_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': info['token'],
+    }
+    x = requests.post(GET_ROOMS_ADMIN_URL, json=data)
+    room_id = json.loads(x.text)['message'][0]['id']
+    print(x.status_code, x.text)
+
+    data = {
+        'token': info['token'],
+        'name': 'cheque1',
+        'room_id': room_id
+    }
+    x = requests.post(ADD_CHEQUE_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': info['token'],
+        'username': 'admin'
+    }
+    x = requests.post(GET_ROOMS_ADMIN_URL, json=data)
+    print(x.status_code, x.text)
+    
+    data = {
+        'token': info['token'],
+        'room_id': room_id
+    }
+    x = requests.post(GET_CHEQUES_URL, json=data)
+    print(x.status_code, x.text)
+
+    data = {
+        'token': info['token'],
+        'room_id': room_id
+    }
+    x = requests.post(DELETE_CHEQUE_URL, json=data)
+    print(x.status_code, x.text)
+
+    x = requests.post(DELETE_ROOMS_URL, json=data)
+    print(x.status_code, x.text)
+    
+    data = {
+        'token': info['token'],
+        'id' : info['id']
+    }
+    requests.post(DELETE_URL, json=data)
+
+
+def main():
+    test_register_and_login()
+    
+    test_add_rooms()
+    #test_join_room()
+    
+    #test_add_cheques()
+
+
+if __name__ == '__main__':
+    main()
