18988be4babab32e7e1086ac3b0e60e20cfe5ca4
Denis Strizhkin <strdenis02@gmail.com>
2023-05-21 22:06:30 +0300

    fix

--- src/api.py
+++ src/api.py
@@ -471,7 +471,7 @@ def cheque_to_dic(id, room_id, is_admin):
 def cheques_admin(user, room_id):
     cheques = []
     for cheque in user.cheques:
-        dic = cheque_to_dic(cheque.id, room_id, is_admin=False)
+        dic = cheque_to_dic(cheque.id, room_id, is_admin=True)
         if dic:
           cheques.append(dic)
     return cheques
